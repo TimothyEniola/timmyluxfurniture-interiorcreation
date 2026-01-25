ac4a136e0cfda389735edffb569dcbf8130377e9
Adewale Ogen <ogenadewalex@gmail.com>
2026-01-25 13:13:31 +0100

    switching to nodejs

--- server/src/middlewares.rs
+++
@@ -1,2 +0,0 @@
-pub mod verify_user;
-pub mod rbac;
--- server/src/types/app_state.rs
+++
@@ -1,8 +0,0 @@
-use sqlx::PgPool;
-use crate::config::Config;
-
-#[derive(Clone)]
-pub struct AppState {
-    pub db: PgPool,
-    pub config: Config, 
-}
